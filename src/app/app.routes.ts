b8fbd6f9caeb249ba10111d0780d2d725dd0a4f0
alice marins <ramoncoost@gmail.com>
2026-08-14 09:20:40 -0300

    feat: implemented home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,18 @@ import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { BooksComponent } from './features/books/books.component';
 import { CreateBookComponent } from './features/create-book/create-book.component';
+import { HomeComponent } from './features/home/pages/home/home.component';
 
 
 
 export const routes: Routes = [
     {
+        path: '', component: HomeComponent
+    },
+    {
         path: '',
         component: MainLayoutComponent,
         children: [
-              {
-                path: '',
-                redirectTo: 'dashboard',
-                pathMatch: 'full'
-            },
             {
                 path: 'dashboard',
                 component: DashboardComponent
