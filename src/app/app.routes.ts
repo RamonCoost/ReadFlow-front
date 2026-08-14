435735e0a627bd4c2d632d12045d2fc269fffc79
alice marins <ramoncoost@gmail.com>
2026-08-14 15:48:53 -0300

    feat: implement register screen and public header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
-import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { BooksComponent } from './features/books/books.component';
 import { CreateBookComponent } from './features/create-book/create-book.component';
-import { HomeComponent } from './features/home/pages/home/home.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { HomeComponent } from './features/home/pages/home.component';
+import { RegisterComponent } from './features/register/pages/register.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 
 
 export const routes: Routes = [
     {
         path: '', component: HomeComponent
+    },
+    {
+        path: 'register', component: RegisterComponent 
     },
     {
         path: '',
---
+++ src/app/features/register/pages/register.component.ts
@@ -0,0 +1,71 @@
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from "@angular/material/button";
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from "@angular/material/input";
+import { PublicHeaderComponent } from "../../../layout/public-header/public-header.component";
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    PublicHeaderComponent,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInput,
+    MatButton,
+    MatIcon,
+    ReactiveFormsModule
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+  form: FormGroup;
+  hide = signal(true);
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get nomeErrors(): string | null {
+    const nomeErroscontrol = this.form.get('nome')
+    if (nomeErroscontrol?.hasError('required')) return 'O campo "Nome" é obrigatório';
+    if (nomeErroscontrol?.hasError('minlength')) return 'O campo "Nome" deve conter no minimo 3 letras';
+    return null;
+  }
+
+  get emailErrors(): string | null {
+    const emailErroscontrol = this.form.get('email')
+    if (emailErroscontrol?.hasError('required')) return 'O campo "Email" é obrigatório';
+    if (emailErroscontrol?.hasError('email')) return 'O campo "email" deve conter um email válido';
+    return null;
+  }
+
+  get senhaErrors(): string | null {
+    const senhaErroscontrol = this.form.get('senha')
+    if (senhaErroscontrol?.hasError('required')) return 'O campo "Senha" é obrigatório';
+    if (senhaErroscontrol?.hasError('minlength')) return 'O campo "Senha" deve conter no minimo 6 números e/ou letras';
+    return null;
+  }
+
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+    } else {
+
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+}
